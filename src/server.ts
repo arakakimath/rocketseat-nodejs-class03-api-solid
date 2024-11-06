cea630a4c26777339940bfb8d80d02fee0877f0a
Matheus Arakaki <arakakimath@gmail.com>
2024-11-06 17:40:26 -0400

    Setting environmental variables and validating it with zod

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './env'
 
 app.get('/',()=>'Hello world!')
 
 app.listen({
     host: '0.0.0.0',
-    port: 3333
+    port: env.PORT
 }).then((address) => {
     console.log(`🚀 HTTP Server Running!\n${address}`)
 })
