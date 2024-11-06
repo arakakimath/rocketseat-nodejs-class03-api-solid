8d03c1d1a284328bb5dde6fc6425478981251bc5
Matheus Arakaki <arakakimath@gmail.com>
2024-11-06 18:59:14 -0400

    eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { app } from './app'
-import { env } from './env'
+import { env } from '@/env'
 
-app.get('/',()=>'Hello world!')
+app.get('/', () => 'Hello world!')
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: env.PORT
-}).then((address) => {
+    port: env.PORT,
+  })
+  .then((address) => {
     console.log(`🚀 HTTP Server Running!\n${address}`)
-})
+  })
