5968a2de90dd527e50f901a43c9dad8c793e5e5d
Matheus Arakaki <arakakimath@gmail.com>
2024-11-08 01:21:17 -0400

    authenticate via http request created

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
@@ -21,8 +21,12 @@ interface RegisterUseCaseResponse {
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) { }
 
-  async execute({ name, email, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
-    const password_hash = await hash(password, 6)
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await bcrypt.hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
