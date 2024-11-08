4e618b8b771fe786508a1245d92bb0683de8c7d4
Matheus Arakaki <arakakimath@gmail.com>
2024-11-08 14:50:35 -0400

    first check-in base file and unit test

--- src/http/use-cases/get-user-profile.spec.ts
+++ src/http/use-cases/get-user-profile.spec.ts
@@ -2,7 +2,6 @@ import { expect, describe, it, beforeEach } from 'vitest'
 
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
-import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { GetUserProfileUseCase } from './get-user-profile'
 import { ResourceNotFoundError } from './errors/resource-not-found'
 
