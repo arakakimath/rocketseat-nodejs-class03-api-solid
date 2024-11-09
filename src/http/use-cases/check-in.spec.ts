4c225374be5c067ba85d13a8c461a467f82f57ff
Matheus Arakaki <arakakimath@gmail.com>
2024-11-08 22:48:25 -0400

    implementing TDD method to do 'user can't checkin twice a day' - RED step

--- src/http/use-cases/check-in.spec.ts
+++ src/http/use-cases/check-in.spec.ts
@@ -1,9 +1,5 @@
 import { expect, describe, it, beforeEach } from 'vitest'
-import { compare } from 'bcryptjs'
 
-import { RegisterUseCase } from './register'
-import { UserAlreadyExistsError } from './errors/user-already-exists'
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
 
@@ -23,5 +19,22 @@ describe('Check-In Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  // TDD -> Test-Driven Development
+  // red, green, refactor
+
+  it('should not be able to check-in more than once a day', async () => {
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,12 +1,12 @@
 import { randomUUID } from 'node:crypto'
-import { User, Prisma, CheckIn } from '@prisma/client'
+import { Prisma, CheckIn } from '@prisma/client'
 
 import { CheckInsRepository } from '../check-ins-repository'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
-  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<User> {
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
