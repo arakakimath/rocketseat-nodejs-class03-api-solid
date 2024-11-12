6a0aaea5c36a1b8785d114771f62416a4cd7a923
Matheus Arakaki <arakakimath@gmail.com>
2024-11-12 14:42:17 -0400

    prisma gyms repository incomplete

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,41 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string): Promise<Gym | null> {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams): Promise<Gym[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
